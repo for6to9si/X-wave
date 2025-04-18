782da97df32cbc73ec152a5f65526a3c7c439299
for6to9si <for6to9si@gmail.com>
2025-04-18 13:50:48 +0700

    added a file S99sing-box, policy settings

--- etc/ndm/netfilter.d/000-swave.sh
+++ etc/ndm/netfilter.d/000-swave.sh
@@ -3,9 +3,6 @@
 # Путь к JSON-файлу с настройками
 SETTING="/opt/etc/swave/settings.json"
 
-# Если переменная не задана, используем /dev/null
-LOG_FILE="${IP_RULES_LOG:-/dev/null}"
-
 get_clean_json() {
   awk '
   BEGIN { in_string = 0 }
@@ -28,29 +25,34 @@ get_clean_json() {
   }' "$1"
 }
 
+js_SETTING=$(get_clean_json "$SETTING" | jq -c '.' 2>/dev/null)
+
+
+IP_RULES_LOG=$(echo "$js_SETTING" | jq -r '.app.log')
+
+# Если переменная не задана, используем /dev/null
+LOG_FILE="${IP_RULES_LOG:-/dev/null}"
+
 restart_script() {
-  exec /bin/sh "\$0" "\$@"
+      "$0" "$@"
+    exit $?
 }
 
-js_SETTING=$(get_clean_json "$SETTING" | jq -c '.' 2>/dev/null)
 
 CMD=$(echo "$js_SETTING" | jq -r '.client.name')
 
 logger -p notice -t "$CMD" "$js_SETTING"
 
 
-if pgrep -f "${CMD} run" > /dev/null
+#if pgrep -f "${CMD} run" > /dev/null //потом заменить
+if pgrep -f "xray run" > /dev/null
 then
-
-# Если переменная не задана, используем /dev/null
-LOG_FILE="${IP_RULES_LOG:-/dev/null}"
-
-busybox ip -4 rule add fwmark 123 lookup 100 >>"$LOG_FILE" 2>&1
-
+  if ! busybox ip -4 rule show | grep -q "fwmark ${table_mark} lookup ${table_id}" >/dev/null 2>&1; then
+  busybox ip -4 rule add fwmark 123 lookup 100 >>"$LOG_FILE" 2>&1
+  fi
 else
 
     sleep 5
 
     restart_script "$@"
 fi
-
