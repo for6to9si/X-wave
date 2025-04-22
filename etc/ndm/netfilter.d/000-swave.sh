100453641043a5dbeeddbb2f4876e1ee079fe046
for6to9si <for6to9si@gmail.com>
2025-04-22 08:46:58 +0700

    Create mangle table chains and rules

--- etc/ndm/netfilter.d/000-swave.sh
+++ etc/ndm/netfilter.d/000-swave.sh
@@ -48,7 +48,7 @@ if pgrep -f "xray run" > /dev/null
 then
   if ! busybox ip -4 rule show | grep -q "fwmark ${table_mark} lookup ${table_id}" >/dev/null 2>&1; then
   # busybox ip -4 rule add fwmark 123 lookup 100 >>"$LOG_FILE" 2>&1
-
+  echo "test"
   fi
 else
 
