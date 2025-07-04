503359161415111b44a392640265c43e9c1b117e
for6to9si <for6to9si@gmail.com>
2025-07-04 13:01:58 +0700

    Enhance system performance through code optimizations

--- etc/ndm/netfilter.d/000-xwave.sh
+++ etc/ndm/netfilter.d/000-xwave.sh
@@ -2,8 +2,8 @@
 
 #!/bin/sh
 
-logger -p notice -t "$(basename "$0")" "Activate '${table}' routing tables"
-logger -p notice -t "$(basename "$0")" "Activate '${type}' routing types"
+#logger -p notice -t "$(basename "$0")" "Activate '${table}' routing tables"
+#logger -p notice -t "$(basename "$0")" "Activate '${type}' routing types"
 
 [ "$table" != "mangle" ] && [ "$table" != "nat" ] && exit 0
 
